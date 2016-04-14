4fb10fa2260bec7e12c3ec24de180d8d0d1ab31b
Seth Baughman <sbaughman1@me.com>
2016-04-14 14:54:53 -0400

    define root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  resources :gifs
-  resources :users
 
+  root   'gifs#index'
   get    '/login'   => 'sessions#new', as: :login
   post   '/login'   => 'sessions#create', as: :login_create
   get    '/logout'  => 'sessions#destroy', as: :logout
+
+  resources :gifs
+  resources :users
 
 end
