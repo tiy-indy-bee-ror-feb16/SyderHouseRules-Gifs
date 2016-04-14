45422bc70b5793a1c1e2db27c265c2085d366d60
Seth Baughman <sbaughman1@me.com>
2016-04-14 15:22:28 -0400

    add validations to gif model

--- app/models/gif.rb
+++ app/models/gif.rb
@@ -1,2 +1,8 @@
 class Gif < ActiveRecord::Base
+  validates :url, :description, presence: true
+  validates :url, length: { minimum: 12, too_short: "not long enough" }
+  validates :url, uniqueness: true
+  validate :url, format: { with: /\Ahttps?:\/\// }
+  validate :url, format: { with: /.gif\Z/ }
+
 end
