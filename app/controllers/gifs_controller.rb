518ed089c514d65f11158860e494c8fb988ab008
Seth Baughman <sbaughman1@me.com>
2016-04-14 15:36:25 -0400

    gif controller actions added

--- app/controllers/gifs_controller.rb
+++ app/controllers/gifs_controller.rb
@@ -1,2 +1,51 @@
 class GifsController < ApplicationController
+  before_action :require_user, except: [:index, :show]
+
+  def index
+    @gifs = Gif.all.order(created_at: :desc).page(params[:page])
+  end
+
+  def show
+    @gif = Gif.find(params[:id])
+  end
+
+  def new
+    @gif = Gif.new
+  end
+
+  def create
+    @gif = Gif.new(gif_params)
+    @gif.user = current_user
+    if @gif.save
+      respond_to do |format|
+        format.html {
+          flash[:success] = "A new gif terrorizes the internet"
+          redirect_to :root
+        }
+        format.js { }
+      end
+    else
+      flash[:danger] = "Invalid, try again"
+      render :new
+    end
+  end
+
+  def destroy
+    gif = Gif.find(params[:id])
+    gif.destroy if gif.user == current_user
+    respond_to do |format|
+      format.html {
+        flash[:success] = "Gif successfully deleted"
+        redirect_to :root
+      }
+      format.js { }
+    end
+  end
+
+  private
+
+  def gif_params
+    params.require(:gif).permit(:url, :description)
+  end
+
 end
--- app/models/gif.rb
+++ app/models/gif.rb
@@ -1,4 +1,5 @@
 class Gif < ActiveRecord::Base
+  belongs_to :user
   validates :url, :description, presence: true
   validates :url, length: { minimum: 12, too_short: "not long enough" }
   validates :url, uniqueness: true
