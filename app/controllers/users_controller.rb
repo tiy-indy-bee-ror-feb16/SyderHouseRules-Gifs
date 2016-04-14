5c94ac2a7637e69c02affcc6cb3115054f59722b
Seth Baughman <sbaughman1@me.com>
2016-04-14 14:15:05 -0400

    add new and create to users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,28 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      respond_to do |format|
+        format.html {
+          flash[:success] = "User account created!"
+          redirect_to :back
+        }
+        format.js { }
+      end
+    else
+      render :new
+    else
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :avatar, :email, :bio, :password, :password_confirmation)
+  end
+
 end
