34e16ec83ad2d11f7fab82ef9d4dd337bc58e58f
Seth Baughman <sbaughman1@me.com>
2016-04-14 16:58:50 -0400

    make gif seeds, styling on gifs index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+100.times do
+  Gif.create!(url: Giphy.random.image_url.to_s, description: "asdfihasd")
+end
