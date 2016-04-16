89c3e335dfb00f1bbd446cc54c4c5fd2ab0949de
Ryder Timberlake <wrtimberlake@gmail.com>
2016-04-16 15:29:56 -0400

    Added test database-populating helper methods and some seeds

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,7 +8,71 @@ require 'rails/test_help'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+  # fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  def self.user_hash(valid: true, inserted_value: nil, key: nil)
+    valid_hash =  {
+                    username: Faker::Name.last_name + "_" + Faker::Name.last_name,
+                    email: Faker::Internet.email,
+                    password: Faker::Internet.password(8)
+                  }
+    if valid
+      valid_hash
+    else
+      if key.nil?
+        valid_hash[valid_hash.to_a.sample.flatten.first] = inserted_value # sets a random key in the hash to inserted_value
+      else
+        valid_hash[key] = inserted_value # sets specific key in the hash to inserted_value
+      end
+    valid_hash
+    end
+  end
+
+  @@users = {
+    valid: [],
+    invalid: {
+      short_password: [],
+      easy_password: [],
+      no_password: [],
+      dup_username: [],
+      no_username: [],
+      no_email: [],
+      dup_email: []
+    }
+  }
+
+  50.times do # valid user
+    @@users[:valid].push(User.create(self.user_hash))
+  end
+
+  3.times do # short password
+    @@users[:invalid][:short_password].push(User.create(self.user_hash(key: :password, inserted_value: Faker::Internet.password(rand(1..7)))))
+  end
+
+  # 3.times do # easy password
+  #   @@users[:invalid][:easy_password].push(User.create(username: Faker::Internet.user_name, email: Faker::Internet.email, password: "baagaaab"))
+  # end
+  #
+  # 3.times do # no password
+  #   @@users[:invalid][:no_password].push(User.create(username: Faker::Internet.user_name, email: Faker::Internet.email))
+  # end
+  #
+  # 3.times do # duplicate username
+  #   @@users[:invalid][:dup_username].push(User.create(username: "Darth_Vader", email: Faker::Internet.email, password: Faker::Internet.password(8)))
+  # end
+  #
+  # 3.times do # no username
+  #   @@users[:invalid][:no_username].push(User.create(email: Faker::Internet.email, password: Faker::Internet.password(8)))
+  # end
+  #
+  # 3.times do # duplicate email
+  #   @@users[:invalid][:dup_email].push(User.create(username: Faker::Internet.user_name, email: "wombat@wombat.com", password: Faker::Internet.password(8)))
+  # end
+  #
+  # 3.times do # no email
+  #   @@users[:invalid][:no_email].push(User.create(username: Faker::Internet.user_name, password: Faker::Internet.password(8)))
+  # end
+
+  @@valid_user = @@users[:valid].sample
+
 end
