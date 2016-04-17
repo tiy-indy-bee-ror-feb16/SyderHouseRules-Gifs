d4e52590cb110492a55cb4889ba9144212c06159
Ryder Timberlake <wrtimberlake@gmail.com>
2016-04-16 20:18:22 -0400

    Began adding helpers for gif seeding in test db

--- test/controllers/gifs_controller_test.rb
+++ test/controllers/gifs_controller_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class GifsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
+
+
+  # test "should create gif" do
+  #   @user = @@valid_user
+  #   params = {
+  #     url: "http://i.imgur.com/Y7syDEa.jpg",
+  #     description: "Just some gif",
+  #   }
+  #
+  #   post :create, params
+  #   gif = assigns(:gif)
+  #   assert gif.persisted?
+  #
   # end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:gifs)
+  end
+
 end
