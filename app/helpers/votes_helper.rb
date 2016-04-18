5845932b9e4108a3cb794218b226ab14fb20da77
Seth Baughman <sbaughman1@me.com>
2016-04-18 14:24:06 -0400

    add vote_arrows partial, vote helper methods, vote button styling

--- app/helpers/votes_helper.rb
+++ app/helpers/votes_helper.rb
@@ -1,2 +1,10 @@
 module VotesHelper
+
+  def has_upvote?(voteable)
+    current_user.voted_for?(voteable) ? "vote-arrow green-arrow" : "vote-arrow"
+  end
+
+  def has_downvote?(voteable)
+    current_user.voted_against?(voteable) ? "vote-arrow red-arrow" : "vote-arrow"
+  end
 end
