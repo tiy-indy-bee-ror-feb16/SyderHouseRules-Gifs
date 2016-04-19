f705a7ade728ff3a1fd3e2060ab1912f70265ea0
Seth Baughman <sbaughman1@me.com>
2016-04-18 21:10:18 -0400

    add routes for tagged gifs, clicking on tag link returns a list of gifs with that tag

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
 
   root   'gifs#index'
-  get    '/login'   => 'sessions#new', as: :login
-  post   '/login'   => 'sessions#create', as: :login_create
-  get    '/logout'  => 'sessions#destroy', as: :logout
+  get    '/login'       => 'sessions#new', as: :login
+  post   '/login'       => 'sessions#create', as: :login_create
+  get    '/logout'      => 'sessions#destroy', as: :logout
   get    '/gifs/:gif_id/upvote'   => 'votes#create', as: :gif_upvote
   get    '/gifs/:gif_id/downvote' => 'votes#destroy', as: :gif_downvote
+  get    'tags/:tag'    => 'gifs#index', as: :tag_gifs
+  get    'users/:user'  => 'gifs#index', as: :user_gifs
 
 
   resources :gifs
