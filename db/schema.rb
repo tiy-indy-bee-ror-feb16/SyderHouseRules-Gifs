5b1e5bb42fa50c04219374780ead296ddddc54a1
Ryder Timberlake <wrtimberlake@gmail.com>
2016-04-20 06:43:29 -0400

    Added table refile_attachments in the schema, added attachment gif showing logic to _gif_show, removed superfluous serve config.serve_static_assets setting from application.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160419174009) do
+ActiveRecord::Schema.define(version: 20160420103801) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,12 @@ ActiveRecord::Schema.define(version: 20160419174009) do
   end
 
   add_index "gifs", ["user_id"], name: "index_gifs_on_user_id", using: :btree
+
+  create_table "refile_attachments", force: :cascade do |t|
+    t.string "namespace", null: false
+  end
+
+  add_index "refile_attachments", ["namespace"], name: "index_refile_attachments_on_namespace", using: :btree
 
   create_table "taggings", force: :cascade do |t|
     t.integer  "tag_id"
